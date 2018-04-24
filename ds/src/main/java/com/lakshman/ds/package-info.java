a2ffa6cbef23287e476fcbeb15aea79190ee86c2
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-04-25 01:19:26 +0530

    master: Added todo list

---
+++ ds/src/main/java/com/lakshman/ds/package-info.java
@@ -0,0 +1,14 @@
+package com.lakshman.ds;
+
+//All basic data structures
+// TODO BST
+// TODO LinkedLists
+// TODO Graphs
+// TODO Queues
+// TODO stack
+// TODO heaps
+// TODO trie
+// TODO Balanced Trees AVL
+// TODO Balanced Trees RedBlack
+// TODO Graphs algorithms
+
