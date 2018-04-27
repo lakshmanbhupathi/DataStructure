b766d54e44bfedc28510da8c724fedc4cabf81f5
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-04-28 00:11:38 +0530

    master:SingleLinkedList: added insert and print methods

--- ds/src/main/java/com/lakshman/ds/linkedlists/SingleLinkedList.java
+++ ds/src/main/java/com/lakshman/ds/linkedlists/SingleLinkedList.java
@@ -10,7 +10,49 @@ public class SingleLinkedList {
     class Node{
         int value;
         Node next;
+
+        Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
     }
 
+    /**
+     * Inserts node into linkedList
+     * @param value
+     * @return
+     */
+    public boolean insertNode(int value){
+        if(head == null){
+            head = new Node(value,null);
+            return true;
+        }
+
+        Node currentNode = head;
+        while(currentNode.next != null){
+            currentNode = currentNode.next;
+        }
+        currentNode.next = new Node(value,null);
+        return true;
+    }
+
+    public void printLinkedList(){
+        Node currentNode = head;
+        while(currentNode != null){
+            System.out.print(currentNode.next == null ? currentNode.value : currentNode.value + " --> ");
+            currentNode = currentNode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+        singleLinkedList.insertNode(1);
+        singleLinkedList.insertNode(2);
+        singleLinkedList.insertNode(3);
+        singleLinkedList.insertNode(4);
+        singleLinkedList.insertNode(5);
+
+        singleLinkedList.printLinkedList();
+    }
 
 }
