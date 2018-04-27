87fe49ef36b9cc73fefc8b6a1e0083779d828a68
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-04-27 23:49:25 +0530

    master: Added SingleLinkedList

---
+++ ds/src/main/java/com/lakshman/ds/linkedlists/SingleLinkedList.java
@@ -0,0 +1,16 @@
+package com.lakshman.ds.linkedlists;
+
+/**
+ * Single Linked list and basic operations
+ *
+ */
+public class SingleLinkedList {
+    private Node head;
+
+    class Node{
+        int value;
+        Node next;
+    }
+
+
+}
