9afe8ec117753d35e436c1b59b99cfccd3a4a6dc
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-04-30 02:53:53 +0530

    master: BinarySearchTree

--- ds/src/main/java/com/lakshman/ds/bst/BinarySearchTree.java
+++ ds/src/main/java/com/lakshman/ds/bst/BinarySearchTree.java
@@ -1,4 +1,17 @@
 package com.lakshman.ds.bst;
 
 public class BinarySearchTree {
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    private Node root;
+
 }
