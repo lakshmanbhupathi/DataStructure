926153479cb1f14317bc0f33ea9e88bac5e4f2a1
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-05-01 17:10:11 +0530

    master: BinarySearchTree : added insert operation

--- ds/src/main/java/com/lakshman/ds/bst/BinarySearchTree.java
+++ ds/src/main/java/com/lakshman/ds/bst/BinarySearchTree.java
@@ -2,16 +2,54 @@ package com.lakshman.ds.bst;
 
 public class BinarySearchTree {
 
-    public static class Node{
+    public static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data){
+        Node(int data) {
             this.data = data;
         }
     }
 
     private Node root;
+
+    public boolean insert(int data) {
+        if (root == null) {
+            root = new Node(data);
+            return true;
+        }
+        return insert(root, data);
+    }
+
+    private boolean insert(Node node, int data) {
+        if (node.data > data) {
+            if (node.left == null) {
+                node.left = new Node(data);
+                return true;
+            }
+            insert(node.left, data);
+        } else {
+            if (node.right == null) {
+                node.right = new Node(data);
+                return true;
+            }
+            insert(node.right, data);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(5);
+        bst.insert(3);
+        bst.insert(2);
+        bst.insert(1);
+        bst.insert(7);
+        bst.insert(6);
+
+        System.out.println("debug");
+    }
 
 }
